42c792838274a126e8ff67231a7c33f4d383df80
vikas kumar <kvikaskumar040@gmail.com>
2026-01-19 13:50:19 -0800

    fix render dist serve

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require("path");
+const cors = require("cors");
+const Dbconnection = require("./config/db.js");
 
-const cors = require('cors')
-const  Dbconnection = require('./config/db.js')
-
- 
-
-
+// DB
 Dbconnection();
+
+
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(require('./Routes/auth'));
-app.use(require('./Routes/createPost'));
-app.use(require('./Routes/user'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//serving frontend
-app.use(express.static(path.join(__dirname,"./Frontend/dist")));
 
-app.get((req,res)=>{
-    res.sendFile(
-        path.join(__dirname,"./Frontend/dist/index.html"),
-        function (err){
-            res.status(500).send(err);
-        }
-    )
-})
+app.use("/auth", require("./Routes/auth"));
+app.use("/post", require("./Routes/createPost"));
+app.use("/user", require("./Routes/user"));
 
-app.listen(PORT,()=>{
-    console.log('server is running on port' + PORT);
-})
 
+
+
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "Frontend", "dist");
+
+  app.use(express.static(frontendPath));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
+// server
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
